a955571d83710fe14fe030ab1c58c75992a31008
jrajan <jinu.p.r@gmail.com>
2020-09-09 22:52:05 +0530

    challenge

---
+++ LeetCode/monthly_challenges/september_2020/9_compare_versions.py
@@ -0,0 +1,96 @@
+"""
+Compare Version Numbers
+Compare two version numbers version1 and version2.
+If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
+
+You may assume that the version strings are non-empty and contain only digits and the . character.
+
+The . character does not represent a decimal point and is used to separate number sequences.
+
+For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+
+You may assume the default revision number for each level of a version number to be 0. For example, version number 3.4 has a revision number of 3 and 4 for its first and second level revision number. Its third and fourth level revision number are both 0.
+
+Example 1:
+
+Input: version1 = "0.1", version2 = "1.1"
+Output: -1
+Example 2:
+
+Input: version1 = "1.0.1", version2 = "1"
+Output: 1
+Example 3:
+
+Input: version1 = "7.5.2.4", version2 = "7.5.3"
+Output: -1
+Example 4:
+
+Input: version1 = "1.01", version2 = "1.001"
+Output: 0
+Explanation: Ignoring leading zeroes, both "01" and "001" represent the same number "1"
+Example 5:
+
+Input: version1 = "1.0", version2 = "1.0.0"
+Output: 0
+Explanation: The first version number does not have a third level revision number, which means its third level revision number is default to "0"
+ 
+
+Note:
+
+Version strings are composed of numeric strings separated by dots . and this numeric strings may have leading zeroes.
+Version strings do not start or end with dots, and they will not be two consecutive dots.
+"""
+
+
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        version1 = version1.split(".")
+        version2 = version2.split(".")
+        import pdb; pdb.set_trace()
+        while version1 and version2:
+            v1 = version1.pop(0)
+            v2 = version2.pop(0)
+            if not int(v1) and not int(v2):
+                continue
+            if len(v1) > 1:
+                v1 = v1.lstrip("0")
+            if len(v2) > 1:
+                v2 = v2.lstrip("0")
+            if int(v1) > int(v2):
+                return 1
+            elif int(v1) < int(v2):
+                return -1
+            else:
+                continue
+        if version1:
+            r = 0
+            while version1:
+                v1 = version1.pop(0)
+                if v1 != '0':
+                    r = 1
+                    break
+            return r
+        if version2:
+            r = 0
+            while version2:
+                v2 = version2.pop(0)
+                if v2 != '0':
+                    r = -1
+                    break
+            return r
+        return 0
+
+# print Solution().compareVersion("0.1", "1.1")
+# print Solution().compareVersion("1.0.1", "1")
+# print Solution().compareVersion("7.5.2.4", "7.5.3")
+# print Solution().compareVersion("1.01", "1.001")
+# print Solution().compareVersion("1.0", "1.0.0")
+# print Solution().compareVersion("1.2", "1.10")
+print Solution().compareVersion(
+    "19.8.3.17.5.01.0.0.4.0.0.0.0.0.0.0.0.0.0.0.0.0.00.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.000000.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.000000",
+    "19.8.3.17.5.01.0.0.4.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0000.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.000000")
