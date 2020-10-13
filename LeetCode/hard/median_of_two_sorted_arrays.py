6f3e06ba81accc9a47fc78b7f2a1bf8c77aa4236
jrajan <jinu.p.r@gmail.com>
2020-10-13 09:45:56 +0530

    hard problem

--- LeetCode/hard/median_of_two_sorted_arrays.py
+++ LeetCode/hard/median_of_two_sorted_arrays.py
@@ -45,9 +45,7 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
-
         def median(arr):
-            import pdb; pdb.set_trace()
             n = len(arr)
             if n % 2 == 1:
                 return arr[n // 2]
@@ -84,6 +82,63 @@ class Solution:
         return median(result)
 
 
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """
+        Take
+        Start with mid which is n1 + n2 + 1 // 2
+        check if the left side length is equal to 
+        """
+        def median(arr):
+            n = len(arr)
+            if n % 2 == 1:
+                return arr[n // 2]
+            else:
+                return (arr[n // 2] + arr[ (n // 2)-1]) / 2
+        if not nums1:
+            return median(nums2)
+        if not nums2:
+            return median(nums1)
+
+        n1 = len(nums1)
+        n2 = len(nums2)
+        min_val = float("-inf")
+        max_val = float("inf")
+
+        if n2 > n1:
+            nums1, nums2 = nums2, nums1
+            n1, n2 = n2, n1
+        import pdb; pdb.set_trace()
+        start, end = 0, n1
+        half_len = (n1 + n2 + 1 ) // 2
+        while start <= end:
+            partition_x = (start + end) // 2
+            partition_y = half_len - partition_x
+            max_left_x = nums1[partition_x-1] if partition_x != 0 else min_val
+            min_right_x = nums1[partition_x] if partition_x != n1 else max_val
+
+            max_left_y = nums2[partition_y-1] if partition_y != 0 else min_val
+            min_right_y = nums2[partition_y] if partition_y != n2 else max_val
+
+            if max_left_x <= min_right_y and max_left_y <= min_right_x:
+                # found the right partition
+                if (n1 + n2) % 2 == 0:
+                    # evenlength
+                    return (max(max_left_x,max_left_y) + min(min_right_y, min_right_x)) / 2
+                else:
+                    return max(max_left_x,max_left_y)
+            elif max_left_x > min_right_y:
+                end = partition_x - 1
+            else:
+                start = partition_x + 1
+
+
+            
+
+
+
+
+
 
 nums1 = [1, 2]
 nums2 = [3, 4]
@@ -91,5 +146,30 @@ print(Solution().findMedianSortedArrays(nums1, nums2))
 nums1 = [1, 3]
 nums2 = [2]
 print(Solution().findMedianSortedArrays(nums1, nums2))
+
+nums1 = [4,5,6,7,8,9]
+nums2 =[1, 2, 3 ]
+"""
+# 1, 2, 3,4 ,5, 6,7,8,9
+start,end = 
+# x [4,5,6]  [7,8,9]
+# y [1, 2]   [3]
+max_left_x 6
+min_left_y 7
+
+max_left_y 2
+min_left_y 3
+
+
+start = 3
+
+loop 2
+
+partionx = 
+
+"""
+
+print(Solution().findMedianSortedArrays(nums1, nums2))
+
 
 
