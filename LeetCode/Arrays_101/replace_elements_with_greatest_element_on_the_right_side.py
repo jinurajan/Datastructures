338b84bed09cd0d7ab034d7c513e74dca7adf7ac
jinurajan <jinu.p.r@gmail.com>
2021-02-07 19:55:30 +0100

    Array 101 problems

---
+++ LeetCode/Arrays_101/replace_elements_with_greatest_element_on_the_right_side.py
@@ -0,0 +1,45 @@
+"""
+Replace Elements with Greatest Element on Right Side
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+Explanation:
+- index 0 --> the greatest element to the right of index 0 is index 1 (18).
+- index 1 --> the greatest element to the right of index 1 is index 4 (6).
+- index 2 --> the greatest element to the right of index 2 is index 4 (6).
+- index 3 --> the greatest element to the right of index 3 is index 4 (6).
+- index 4 --> the greatest element to the right of index 4 is index 5 (1).
+- index 5 --> there are no elements to the right of index 5, so we put -1.
+
+Input: arr = [400]
+Output: [-1]
+Explanation: There are no elements to the right of index 0.
+
+Constraints:
+
+1 <= arr.length <= 104
+1 <= arr[i] <= 105
+
+"""
+from typing import List
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        greatest_val = -1
+        for i in range(len(arr)-1,-1,  -1):
+            tmp = greatest_val
+            greatest_val = max(greatest_val, arr[i])
+            arr[i] = tmp
+        return arr
+
+
+arr = [17,18,5,4,6,1]
+print(Solution().replaceElements(arr))
+arr = [400]
+print(Solution().replaceElements(arr))
+
+
+
