36b0aac3d0971d6acd3ddb03f5ceb5cdf5a0ee1a
jinurajan <jinu.p.r@gmail.com>
2021-02-15 22:03:41 +0100

    Facebook Problems

---
+++ LeetCode/facebook/recursion/permutations.py
@@ -0,0 +1,42 @@
+"""
+Permutations
+
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Input: nums = [1]
+Output: [[1]]
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+
+"""
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(first=0):
+            if first == n:
+                output.append(nums[:])
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first+1)
+                nums[first], nums[i] = nums[i], nums[first]
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
+
+
+nums = [1,2,3]
+print(Solution().permute(nums))
