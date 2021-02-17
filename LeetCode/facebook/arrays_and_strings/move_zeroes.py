c793f0810e55c60def4f379779ac5d5ab64516fe
jinurajan <jinu.p.r@gmail.com>
2021-02-17 19:57:29 +0100

    Facebook Problems

---
+++ LeetCode/facebook/arrays_and_strings/move_zeroes.py
@@ -0,0 +1,23 @@
+"""
+Move Zeroes
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        start = 0
+        for idx, n in enumerate(nums):
+            if n != 0:
+                nums[start], nums[idx] = nums[idx], nums[start]
+                start += 1
+        return nums
