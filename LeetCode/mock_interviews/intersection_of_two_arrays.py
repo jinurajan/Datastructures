6b02f07e61fa93a6f7788978f5b761437c5cf5eb
jinurajan <jinu.p.r@gmail.com>
2021-02-17 22:43:36 +0100

    Mock interview Problems

---
+++ LeetCode/mock_interviews/intersection_of_two_arrays.py
@@ -0,0 +1,50 @@
+"""
+Intersection of Two Arrays II
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+"""
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) < len(nums2):
+            nums1, nums2 = nums2, nums1
+        hash_map = defaultdict(list)
+        for idx, n in enumerate(nums1):
+            hash_map[n].append(idx)
+        res = []
+        for n in nums2:
+            if n not in hash_map or not hash_map[n]:
+                continue
+            hash_map[n].pop()
+            res.append(n)
+        return res
+
+nums1=[1,2]
+nums2=[1,1]
+print(Solution().intersect(nums1, nums2))
+nums1= [4,7,9,7,6,7]
+nums2 = [5,0,0,6,1,6,2,2,4]
+print(Solution().intersect(nums1, nums2))
---
+++ LeetCode/mock_interviews/longest_k_distinct_substring.py
@@ -0,0 +1,65 @@
+"""
+Longest Substring with At Most K Distinct Characters
+
+Given a string s and an integer k, return the length of the longest substring of s that contains at most k distinct characters.
+
+
+
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: The substring is "ece" with length 3.
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: The substring is "aa" with length 2.
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+0 <= k <= 50
+
+"""
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        if k == 0 or not s:
+            return 0
+        start = 0
+        end = 0
+        max_val = 1
+        hash_map = {}
+        while end < len(s):
+            hash_map[s[end]] = end
+            end += 1
+            if len(hash_map) == k+1:
+                idx = min(hash_map.values())
+                del hash_map[s[idx]]
+                start = idx + 1
+            max_val = max(max_val, end-start)
+        return max_val
+
+
+
+# s = "eceba"
+# k = 2
+# print(Solution().lengthOfLongestSubstringKDistinct(s, k))
+
+
+# s = "aa"
+# k = 1
+# print(Solution().lengthOfLongestSubstringKDistinct(s, k))
+
+# s ='a'
+# k = 0
+# print(Solution().lengthOfLongestSubstringKDistinct(s, k))
+#
+# s = 'aba'
+# k = 1
+# print(Solution().lengthOfLongestSubstringKDistinct(s, k))
+
+s = "bacc"
+k = 2
+print(Solution().lengthOfLongestSubstringKDistinct(s, k))
