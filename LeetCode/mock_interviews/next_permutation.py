a20234cf5044e00fd569c42c7b6a8300ae6e7303
jinurajan <jinu.p.r@gmail.com>
2021-02-25 00:21:36 +0100

    mock problems

--- LeetCode/mock_interviews/next_permutation.py
+++ LeetCode/mock_interviews/next_permutation.py
@@ -38,17 +38,22 @@ class Solution:
         i = n - 2
 
         def reverse(nums, start):
-            for i in range(start, n-1):
-                nums[start], nums[n-1-start] = nums[n-1-start], nums[start]
-        while i >= 0 and nums[i+1] <= nums[i]:
+            i = start
+            j = n - 1
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
+        while i >= 0 and nums[i + 1] <= nums[i]:
             i -= 1
         if i >= 0:
-            j = n - 1
+            j = len(nums) - 1
             while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
-            reverse(nums, i+1)
-        return nums
+        reverse(nums, i + 1)
+
 nums = [1,2,3]
 print(Solution().nextPermutation(nums))
 nums = [3,2,1]
