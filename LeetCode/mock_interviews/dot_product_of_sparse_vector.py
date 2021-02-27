1615a1e29ba103f1be07b55f4112ee89774c6e3f
jinurajan <jinu.p.r@gmail.com>
2021-02-27 14:04:29 +0100

    mock problems

---
+++ LeetCode/mock_interviews/dot_product_of_sparse_vector.py
@@ -0,0 +1,64 @@
+"""
+Dot Product of Two Sparse Vectors
+Given two sparse vectors, compute their dot product.
+
+Implement class SparseVector:
+
+SparseVector(nums) Initializes the object with the vector nums
+dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+
+Follow up: What if only one of the vectors is sparse?
+
+Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+Output: 8
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+Example 2:
+
+Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+Output: 0
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 0*0 + 1*0 + 0*0 + 0*0 + 0*2 = 0
+Example 3:
+
+Input: nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+Output: 6
+
+
+Constraints:
+
+n == nums1.length == nums2.length
+1 <= n <= 10^5
+0 <= nums1[i], nums2[i] <= 100
+"""
+
+
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nonzeros = {}
+        for idx, n in enumerate(nums):
+            self.nonzeros[idx] = n
+
+            # Return the dotProduct of two sparse vectors
+
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        for i, n in self.nonzeros.items():
+            if i in vec.nonzeros:
+                result += n * vec.nonzeros[i]
+        return result
+
+
+class SparseVector1:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        prodt = 0
+        for first, secnd in zip(self.nums, vec.nums):
+            if not first or not secnd:
+                continue
+            prodt += first * secnd
+        return prodt
