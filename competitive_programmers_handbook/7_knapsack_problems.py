288ac077203fca33530d8ee755ee1c168aab744d
jinurajan <jinu.p.r@gmail.com>
2021-02-28 22:58:56 +0100

    competitive programmers handbook chapter7

--- competitive_programmers_handbook/7_knapsack_problems.py
+++ competitive_programmers_handbook/7_knapsack_problems.py
@@ -7,20 +7,23 @@ Given a list of weights [w1,w2,...,wn], determine all sums that can be construct
 # for (int x = 0; x <= W; x++) {
 # if (x-w[k] >= 0) possible[x][k] |= possible[x-w[k]][k-1]; possible[x][k] |= possible[x][k-1];
 # } }
-
 def possible_sums(weights):
-    n = len(weights)
-    W = sum(weights) + 1
-    dp = [[0 for i in range(W)] for j in range(n)]
-    w = [i for i in range(max(weights))]
-    print(dp)
-    for x in range(1, W):
-        for k in range(n):
-            # print(x, weights[k])
-            if x - w[k] >=0:
-                print(x, w[k])
-                dp[x][k] |= dp[x-w[k]][k-1]
-            dp[x][k] |= dp[x][k-1]
-    print(dp)
+    max_sum = sum(weights)+ 1
+    w = len(weights)
+    possible =[0 for i in range(max_sum)]
+    possible[0] = 1
+    for k in range(w):
+        for x in range(max_sum-1, -1, -1):
+            if possible[x]:
+                possible[x+weights[k]] = 1
+    result = [idx for idx, n in enumerate(possible) if n==1 and idx!=0]
+    return result
+
+
+
+
+
+
+
 weights = [1,3,3, 5]
 print(possible_sums(weights))
