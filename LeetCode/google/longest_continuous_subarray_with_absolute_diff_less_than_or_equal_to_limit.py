06fe78dce8b14e835897b0ffaf9d5fa600553f92
jinurajan <jinu.p.r@gmail.com>
2021-03-14 23:32:57 +0100

    mock problems

---
+++ LeetCode/google/longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
@@ -0,0 +1,60 @@
+"""
+Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+
+Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+
+
+
+Example 1:
+
+Input: nums = [8,2,4,7], limit = 4
+Output: 2
+Explanation: All subarrays are:
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4.
+Therefore, the size of the longest subarray is 2.
+Example 2:
+
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+Example 3:
+
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+0 <= limit <= 109
+"""
+from collections import deque
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        minq, maxq = deque(), deque()
+        i = 0
+        res = 0
+        for j, n in enumerate(nums):
+            while minq and minq[-1] > n: minq.pop()
+            while maxq and maxq[-1] < n: maxq.pop()
+            minq.append(n)
+            maxq.append(n)
+
+            while i < len(nums) and minq and maxq and maxq[0] - minq[0] > limit:
+                if maxq[0] == nums[i]: maxq.popleft()
+                if minq[0] == nums[i]: minq.popleft()
+                res = max(res, j - i)
+                i += 1
+        return max(res, len(nums) - i)
