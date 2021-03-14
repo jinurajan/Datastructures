3cb10d08b1354603063708a4f66948953d1e7316
jinurajan <jinu.p.r@gmail.com>
2021-03-14 12:16:02 +0100

    monthly challenge

---
+++ LeetCode/monthly_challenges/2021/march/14_swapping_nodes_in_a_linked_list.py
@@ -0,0 +1,54 @@
+"""
+Swapping Nodes in a Linked List
+
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+Example 3:
+
+Input: head = [1], k = 1
+Output: [1]
+Example 4:
+
+Input: head = [1,2], k = 1
+Output: [2,1]
+Example 5:
+
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        list_nodes = []
+        node = head
+        while node:
+            list_nodes.append(node.val)
+            node = node.next
+        list_nodes[ k -1], list_nodes[-k] = list_nodes[-k], list_nodes[ k -1]
+        dummy_node = ListNode()
+        node = dummy_node
+        while list_nodes:
+            node.next = ListNode(list_nodes.pop(0))
+            node = node.next
+        return dummy_node.next
