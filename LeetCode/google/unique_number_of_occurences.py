4c044df6e19c3de3b42b346070eaa1a319940a16
jinurajan <jinu.p.r@gmail.com>
2021-03-17 19:44:54 +0100

    mock interviews

--- LeetCode/google/unique_number_of_occurences.py
+++ LeetCode/google/unique_number_of_occurences.py
@@ -28,8 +28,6 @@ Constraints:
 from typing import List
 
 from collections import Counter
-
-
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         counter = Counter(arr)
