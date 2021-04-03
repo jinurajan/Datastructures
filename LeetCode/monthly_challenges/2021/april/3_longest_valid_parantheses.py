6853666fe5f1b44cd98d4142dca98035ea71c2ce
jinurajan <jinu.p.r@gmail.com>
2021-04-03 22:41:54 +0200

    monthly challenge

---
+++ LeetCode/monthly_challenges/2021/april/3_longest_valid_parantheses.py
@@ -0,0 +1,47 @@
+"""
+ Longest Valid Parentheses
+
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+
+Constraints:
+
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.
+
+"""
+from collections import deque
+
+
+class Solution1:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+        stack = deque([-1])
+        max_val = 0
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_val = max(max_val, i - stack[-1])
+        return max_val
