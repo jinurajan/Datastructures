10ff1426ffe393c58bcfd283d02909e74eddfd14
jinurajan <jinu.p.r@gmail.com>
2021-05-13 16:20:08 +0200

    amazon mockquestions

---
+++ LeetCode/hard/word_ladder.py
@@ -0,0 +1,130 @@
+"""
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: 0
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+
+
+Constraints:
+
+1 <= beginWord.length <= 10
+endWord.length == beginWord.length
+1 <= wordList.length <= 5000
+wordList[i].length == beginWord.length
+beginWord, endWord, and wordList[i] consist of lowercase English letters.
+beginWord != endWord
+All the words in wordList are unique.
+
+"""
+
+from collections import deque
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        q = deque([(beginWord, 1)])
+        visited = set()
+        graph = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                graph[word[:i] + '_' + word[i + 1:]].append(word)
+        while q:
+            word, level = q.popleft()
+            if word == endWord:
+                return level
+            l = len(q)
+            if word not in visited:
+                visited.add(word)
+                for i in range(len(word)):
+                    neighbors = word[:i] + '_' + word[i + 1:]
+                    for nei in graph[neighbors]:
+                        q.append((nei, level + 1))
+        return 0
+
+from collections import defaultdict
+class Solution(object):
+    def __init__(self):
+        self.length = 0
+        # Dictionary to hold combination of words that can be formed,
+        # from any given word. By changing one letter at a time.
+        self.all_combo_dict = defaultdict(list)
+
+    def visitWordNode(self, queue, visited, others_visited):
+        current_word, level = queue.popleft()
+        for i in range(self.length):
+            # Intermediate words for current word
+            intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+            # Next states are all the words which share the same intermediate state.
+            for word in self.all_combo_dict[intermediate_word]:
+                # If the intermediate state/word has already been visited from the
+                # other parallel traversal this means we have found the answer.
+                if word in others_visited:
+                    return level + others_visited[word]
+                if word not in visited:
+                    # Save the level as the value of the dictionary, to save number of hops.
+                    visited[word] = level + 1
+                    queue.append((word, level + 1))
+        return None
+
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+
+        # Since all words are of same length.
+        self.length = len(beginWord)
+
+        for word in wordList:
+            for i in range(self.length):
+                # Key is the generic word
+                # Value is a list of words which have the same intermediate generic word.
+                self.all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+
+
+        # Queues for birdirectional BFS
+        queue_begin = collections.deque([(beginWord, 1)]) # BFS starting from beginWord
+        queue_end = collections.deque([(endWord, 1)]) # BFS starting from endWord
+
+        # Visited to make sure we don't repeat processing same word
+        visited_begin = {beginWord: 1}
+        visited_end = {endWord: 1}
+        ans = None
+
+        # We do a birdirectional search starting one pointer from begin
+        # word and one pointer from end word. Hopping one by one.
+        while queue_begin and queue_end:
+
+            # One hop from begin word
+            ans = self.visitWordNode(queue_begin, visited_begin, visited_end)
+            if ans:
+                return ans
+            # One hop from end word
+            ans = self.visitWordNode(queue_end, visited_end, visited_begin)
+            if ans:
+                return ans
+
+        return 0
+
+
+
+
+
