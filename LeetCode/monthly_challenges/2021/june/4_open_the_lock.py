51387c0691e6be01fb9fc413c54a10de673d560d
jinurajan <jinu.p.r@gmail.com>
2021-06-04 23:28:24 +0200

    educative and mock interviews

--- LeetCode/monthly_challenges/2021/june/4_open_the_lock.py
+++ LeetCode/monthly_challenges/2021/june/4_open_the_lock.py
@@ -1,7 +1,7 @@
 """
 Open the Lock
 
-you have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
 
 The lock initially starts at '0000', a string representing the state of the 4 wheels.
 
@@ -43,15 +43,13 @@ Constraints:
 deadends[i].length == 4
 target.length == 4
 target will not be in the list deadends.
-target and deadends[i] consist of digits only
+target and deadends[i] consist of digits only.
 """
 
-from collections import deque
-
+from typing import List
 
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-
         def neighbors(node):
             for i in range(4):
                 x = int(node[i])
@@ -61,17 +59,19 @@ class Solution:
 
         if target == "0000":
             return 0
+        q = [("0000", 0)]
         deadends = set(deadends)
-        q = deque([("0000", 0)])
         seen = {"0000"}
         while q:
-            node, depth = q.popleft()
+            node, dist = q.pop(0)
             if node == target:
-                return depth
+                return dist
             if node in deadends:
                 continue
+
             for n in neighbors(node):
                 if n not in seen:
                     seen.add(n)
-                    q.append((n, depth + 1))
+                    q.append((n, dist+1))
         return -1
+
