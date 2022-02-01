6bb6487c3d64eac313fc6ab1189e8a51017c80c5
Jinu Palliyil Rajan <jpalliyilrajan@c02g719cml87.home>
2022-02-01 22:52:48 +0000

    decode ways in topdown dp

--- LeetCode/medium/decode_ways.py
+++ LeetCode/medium/decode_ways.py
@@ -46,6 +46,22 @@ from functools import lru_cache
 
 
 class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s:
+            return 0
+        dp = [0 for _ in range(len(s)+1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+
+        for i in range(2, len(dp)):
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+            two_digit = int(s[i-2:i])
+            if two_digit >= 10 and two_digit <= 26:
+                dp[i] += dp[i-2]
+        return dp[-1]
+
+class Solution:
 
     @lru_cache(maxsize=None)
     def recursive_with_memo(self, index, s) -> int:
