2fd062b0e96cfb65c4f7a8d087a34649c7f35da8
jrajan <jinu.p.r@gmail.com>
2023-06-07 20:49:34 +0100

    contains duplicate 50th problem

--- LeetCode/blind_75/contains_duplicate.py
+++ LeetCode/blind_75/contains_duplicate.py
@@ -8,9 +8,20 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # hash map
         visited = set()
         for num in nums:
             if num in visited:
                 return True
             visited.add(num)
+        return False
+    
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        n = len(nums)
+        for i in range(n-1):
+            if nums[i] == nums[i+1]:
+                return True
         return False
